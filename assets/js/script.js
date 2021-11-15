b8346276ac04cde3f62e287fa7e9d03c6abfc3a0
Rafael Tavares <rafatavares03@hotmail.com>
2021-11-14 21:23:15 -0300

    js ajustment

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,4 @@
 const emailRegex = /^[a-z0-9.]+@[a-z0-9]+\.[a-z0-9]+(\.[a-z0-9]+)?$/i;
-const passwordRegex = /^([a-z0-9$@&]+){8,}/i;
 
 document.querySelector('#email').addEventListener('keyup', (e) => {
   const element = e.target;
